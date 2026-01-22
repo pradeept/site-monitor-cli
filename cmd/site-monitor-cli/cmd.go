98882ae18688b7d0a755d91160c14fb6ad50f83d
pradeept <pradeep.tarakar17@gmail.com>
2026-01-22 23:34:52 +0530

    refactor: add comments in cmd.go for readability

--- cmd/site-monitor-cli/cmd.go
+++ cmd/site-monitor-cli/cmd.go
@@ -7,15 +7,17 @@ import (
 )
 
 func ConfigCommands() {
+	// sub-command add
 	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
 	addWebsite := addCmd.String("website", "", "Specify the url (ex: https://www.google.com)")
 	callTime := addCmd.String("time", "", "Specify the time to call in seconds (ex: 2)")
 
+	// sub-command remove
 	removeCmd := flag.NewFlagSet("remove", flag.ExitOnError)
 	removeWebsite := removeCmd.String("website", "", "Specify the url (ex: https://www.google.com)")
 
+	// no arguments are passed
 	if len(os.Args) < 2 {
-
 		fmt.Println("Expected an argument, passed 0")
 		os.Exit(1)
 	}
@@ -23,11 +25,13 @@ func ConfigCommands() {
 	switch os.Args[1] {
 
 	case "add":
+		// parse flags for add sub-command
 		addCmd.Parse(os.Args[2:])
 		fmt.Println("[Add] Website: ", *addWebsite)
 		fmt.Println("Time: ", *callTime)
 
 	case "remove":
+		// parse flags for remove sub-command
 		removeCmd.Parse(os.Args[2:])
 		fmt.Println("[Remove] Website: ", *removeWebsite)
 
